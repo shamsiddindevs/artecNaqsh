c67e9d6be17671a475efe1eeca2491c0cd2d7a91
shamsiddin <devshamsiddin@gmain.com>
2024-10-25 14:47:34 +0500

    adding

---
+++ js/main.js
@@ -0,0 +1,325 @@
+/*  ---------------------------------------------------
+    Theme Name: Staging
+    Description: Staging bootstrap tamplate
+    Author: Colorib
+    Author URI: https://www.colorib.com/
+    Version: 1.0
+    Created: Colorib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Canvas Menu
+    $(".canvas__open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("active");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("active");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*-----------------------
+        Hero Slider
+    ------------------------*/
+    $(".hero__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: true,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'><i/>", "<i class='fa fa-angle-right'><i/>"],
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1500,
+        autoHeight: false,
+        autoplay: true,
+        mouseDrag: false,
+        onInitialized: function (e) {
+            var a = this.items().length;
+            $("#snh-1").html("<span>01</span><span>" + "0" + a + "</span>");
+            var presentage = Math.round((100 / a));
+            $('.slider__progress span').css("width", presentage + "%");
+        }
+    }).on("changed.owl.carousel", function (e) {
+        var b = --e.item.index,
+            a = e.item.count;
+        $("#snh-1").html("<span> " + "0" + (1 > b ? b + a : b > a ? b - a : b) + "</span><span>" + "0" + a + "</span>");
+
+        var current = e.page.index + 1;
+        var presentage = Math.round((100 / e.page.count) * current);
+        $('.slider__progress span').css("width", presentage + "%");
+    });
+
+    /*--------------------------
+        Project Slider
+    ----------------------------*/
+    $(".project__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 4,
+        dots: false,
+        nav: true,
+        navText: ["<span class='arrow_carrot-left'><span/>", "<span class='arrow_carrot-right'><span/>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false,
+        responsive: {
+            320: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
+
+    /*--------------------------
+        Modal Slider
+    ----------------------------*/
+    $(".modal__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ["<span class='arrow_carrot-left'><span/>", "<span class='arrow_carrot-right'><span/>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false,
+        
+    });
+
+
+    /*-----------------------------
+        Testimonial Slider
+    -------------------------------*/
+    $('.testimonial__carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        // autoplay:true,
+        asNavFor: '.testimonial__client',
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"><i></i></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"><i></i></i></button>',
+      });
+      $('.testimonial__client').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.testimonial__carousel',
+        arrows: false,
+        variableWidth: true,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                variableWidth: false
+              }
+            }
+          ]
+        
+      });
+
+    /*---------------------------------
+        Logo Carousel
+    ----------------------------------*/
+    $(".logo__carousel").owlCarousel({
+        loop: true,
+        margin: 80,
+        items: 5,
+        dots: false,
+        nav: false,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            320: {
+                items: 2
+            },
+            576: {
+                items: 3
+            },
+            992: {
+                items: 5
+            },
+        }
+    });
+
+    /*------------------
+        Counter
+    --------------------*/
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+})(jQuery);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+var modal5 = document.getElementById("myModal5");
+var modal6 = document.getElementById("myModal6");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
+var btn6 = document.getElementById("myBtn6");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+var span3 = document.getElementsByClassName("close3")[0];
+var span4 = document.getElementsByClassName("close4")[0];
+var span5 = document.getElementsByClassName("close5")[0];
+var span6 = document.getElementsByClassName("close6")[0];
+
+// When the user clicks on the button, open the modal
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+    btn2.onclick = function() {
+        modal2.style.display = "block";
+    }
+    btn3.onclick = function() {
+        modal3.style.display = "block";
+    }
+    btn4.onclick = function() {
+    modal4.style.display = "block";
+    }
+    btn5.onclick = function() {
+    modal5.style.display = "block";
+    }
+    btn6.onclick = function() {
+    modal6.style.display = "block";
+    }
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+span4.onclick = function() {
+    modal4.style.display = "none";
+}
+span5.onclick = function() {
+    modal5.style.display = "none";
+}
+span6.onclick = function() {
+    modal6.style.display = "none";
+}
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
+  if (event.target == modal6) {
+    modal6.style.display = "none";
+  }
+}
+
+
+// email js
+
+function sendEmail() {
+    let params = {
+      subject: "Naqsh form",
+      sender: document.getElementById("name").value,
+      phone: document.getElementById("email").value,
+      email: document.getElementById("phone").value,
+      message: document.getElementById("message").value,
+    };
+    emailjs.send("service_jcoke8p", "template_lybr56n", params).then(
+      function (response) {
+        alert("Email sent successfully");
+        // You can add any success handling here
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        document.getElementById("phone").value = "";
+      },
+      function (error) {
+        alert("Email sending failed");
+        // You can add any error handling here
+      }
+    );
+  }
---
+++ readme.txt
@@ -0,0 +1,50 @@
+===============================================================================
+                                  ARTEC NAQSH
+===============================================================================
+
+Welcome to the Artec Naqsh Project Repository!
+
+ABOUT ARTEC NAQSH:
+------------------
+Artec Naqsh is a leading architecture and design firm based in Tashkent, Uzbekistan, specializing in creating unique and culturally enriched environments. Our projects span across residential, commercial, and public sectors, integrating modern design with traditional Uzbek aesthetics.
+
+This repository contains resources, documentation, and project files related to our architectural designs, interior projects, and collaborations.
+
+DIRECTORY STRUCTURE:
+--------------------
+/docs       - Project documentation and manuals.
+/designs    - Architectural and interior design files.
+/assets     - Images, logos, and other media used in projects.
+/scripts    - Automation scripts and utilities.
+
+USAGE:
+------
+The files and resources contained in this repository are intended for collaboration and reference for Artec Naqsh projects. Please ensure you have the necessary permissions to use or modify the content.
+
+CONTRIBUTING:
+-------------
+We welcome contributions from our team members and collaborators. To contribute:
+1. Fork the repository.
+2. Create a new branch for your feature or fix.
+3. Commit your changes.
+4. Push to the branch.
+5. Submit a pull request.
+
+CONTACT INFORMATION:
+--------------------
+Artec Naqsh
+123 Design Street, Tashkent, Uzbekistan
+Email: contact@artecnaqsh.uz
+Phone: +998 99 473 16 50
+
+For more information, please visit our website: https://www.artecnaqsh.uz/
+
+COPYRIGHT AND LICENSE:
+----------------------
+© [Year] Artec Naqsh. All rights reserved.
+Content in this repository is proprietary to Artec Naqsh unless otherwise specified. Use and distribution are subject to the terms specified in the LICENSE file.
+
+Thank you for your interest in Artec Naqsh!
+
+===============================================================================
+
